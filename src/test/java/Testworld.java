d6453fb2ecf1d9b4657c0434dbd581ed028fad44
unknown <mtarab@rediffmail.com>
2021-10-22 06:03:07 -0500

    change from main to test folder

--- src/test/java/Testworld.java
+++ src/test/java/Testworld.java
@@ -1,19 +1,22 @@
-package Maven.com.git;
+
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class WebGit {
-
+public class Testworld {
+	
 	public static String browser="chrome";
 	public static WebDriver driver;
 	//System.setProperty("webdriver.chrome.driver", "C:\\Users\\Md Tarab\\eclipse-workspace\\SeleniumPractise\\SelPro\\Driver\\chromedriver.exe");
 		
-
-	public static void setup()
+@BeforeSuite
+	public void setup()
 		{
 	if(browser.equals("chrome"))
 		{
@@ -25,29 +28,23 @@ public class WebGit {
 			WebDriverManager.firefoxdriver().setup();
 			driver= new FirefoxDriver();
 		}
-		}
 
-		public static void logk()
-		{
-		driver.get("https://www.way2automation.com/");
-		String title=driver.getTitle();
-		System.out.println(title.length());
-		System.out.println(title);
-		System.out.println(title.charAt(1));
-		
 		}
+@Test
+public void logk()
+{
+driver.get("https://www.way2automation.com/");
+String title=driver.getTitle();
+System.out.println(title.length());
+System.out.println(title);
+System.out.println(title.charAt(1));
 
-		public static void down()
-		{
-		driver.close();//current browser window 
-		driver.quit();//current browser window + all related tabs of browser   
-		}
-		
-		public static void main(String[] args) {
-			setup();
-			logk();
-			down();
-		}
-	}
+}
+@AfterSuite
+public void down()
+{
+driver.close();//current browser window 
+driver.quit();//current browser window + all related tabs of browser   
+}
 
-
+}
